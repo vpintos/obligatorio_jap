e8f9bd91587bae79734d7236dbf307e511e81de9
vpintos <69024873+vpintos@users.noreply.github.com>
2020-08-10 23:14:00 -0300

    Json para la lista de productos

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,14 @@
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsinfoArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProductsinfo(productsinfoArray);
+        }
+    });
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,43 @@
+var productsArray = [];
+
+function showProducts(array) {
+
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+        htmlContentToAppend += `
+        <a href="product-info.html" class="card mb-4 shadow-sm custom-card">
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name + `</h4
+                        <small class="text-muted">` + product.soldCount + ` artículos</small>
+                    </div>
+                    <p> `+ product.description + `</p>
+                </div>
+            </div>
+        </div>
+        </a>
+        `
+
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProducts(productsArray);
+        }
+    });
 });
