fc45389af0bc48aaffcbb8105f931985e62977a2
vpintos <69024873+vpintos@users.noreply.github.com>
2020-08-14 02:51:07 -0300

    google button 2

--- js/login.js
+++ js/login.js
@@ -56,7 +56,23 @@ function password_Verify() {
   }
 }
  
-
+function onSuccess(googleUser) {
+  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+  console.log(error);
+}
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
