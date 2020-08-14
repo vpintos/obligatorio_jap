ccb11481197dbb0b2a221fbb2d8359780ca8629e
vpintos <69024873+vpintos@users.noreply.github.com>
2020-08-14 00:48:17 -0300

    Fullworking login (dab)

--- js/login.js
+++ js/login.js
@@ -1,25 +1,60 @@
-var email = document.forms["form"]["email"].value;
-var password = document.forms["form"]["password"].value;
+var email = document.forms["form"]["email"];
+var password = document.forms["form"]["password"];
 
 var email_error = document.getElementById("email_error");
 var password_error = document.getElementById("password_error");
+var password_numero = document.getElementById("password_numero");
 
 
-function validate() {
-    if (email == "" || email.value == null) {
+email.addEventListener("input", email_Verify);
+password.addEventListener("input", password_Verify);
+
+
+let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+function validated() {
+    if (email.value.length == 0 || !email.value.match(regExp)) {
       email.style.border = "1px solid red";
+      email_error.style.display = "block";
       email.focus();
-      return false;
+      return false; 
     }
 
-    if (x == "" || x == null) {
-      alert("Debes ingresar una contraseña");
+    if (password.value.length == 0) {
+      password.style.border = "1px solid red";
+     password_error.style.display = "block";
+      password.focus();
       return false;
   
     }
-
+    if (password.value.length < 8) {
+      password.style.border = "1px solid red";
+     password_numero.style.display = "block";
+      password.focus();
+      return false;
+    }
+    
 }
-  
+
+
+
+function email_Verify() {
+ 
+  if (email.value.length >= 1 && email.value.match(regExp)) {
+    email.style.border = "1px solid silver";
+    email_error.style.display = "none";
+    return true;
+    }
+}
+
+function password_Verify() {
+  if (password.value.length >= 8) {
+    password.style.border = "1px solid silver";
+   password_error.style.display = "none";
+   password_numero.style.display = "none";
+     return true;
+  }
+}
  
 
 
