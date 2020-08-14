3c9bf333197a35f29809d34e370b07950e11eacb
vpintos <69024873+vpintos@users.noreply.github.com>
2020-08-14 01:18:06 -0300

    js/init: create

--- js/init.js
+++ js/init.js
@@ -46,5 +46,7 @@ var getJSONData = function(url){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-  
+if(!sessionStorage.getItem('usuario')){
+	window.location.replace('login.html');
+}
 });
