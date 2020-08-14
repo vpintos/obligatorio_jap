e9cdd8a629fa2bbe716ddf6971d956ed62d9afc3
vpintos <69024873+vpintos@users.noreply.github.com>
2020-08-13 22:09:29 -0300

    js/login: create

--- js/login.js
+++ js/login.js
@@ -1,30 +1,26 @@
+var email = document.forms["form"]["email"].value;
+var password = document.forms["form"]["password"].value;
+
+var email_error = document.getElementById("email_error");
+var password_error = document.getElementById("password_error");
+
 
 function validate() {
-    var x = document.forms["form"]["exampleInputEmail1"].value;
-    if (x == "" || x == null) {
-      alert("Debes ingresar un correo electrónico");
+    if (email == "" || email.value == null) {
+      email.style.border = "1px solid red";
+      email.focus();
       return false;
     }
-    var x = document.forms["form"]["exampleInputPassword1"].value;
+
     if (x == "" || x == null) {
       alert("Debes ingresar una contraseña");
       return false;
   
     }
-    else{
-    $("form").submit();
-  }
+
 }
   
-  
-  function soloNumeros() {
-        myVar = [8, 9, 13, 37, 38, 39, 40, 46, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 116 ]
-        if(!(myVar.includes(event.keyCode))) {
-          alert("Solo se admiten números");
-          return false;
-        }
-  }
-   
+ 
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
